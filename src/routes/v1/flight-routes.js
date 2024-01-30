5b826175b539c937020e2b098f434d4f0d32a241
Prateem1 <prateema.adhikari@gmail.com>
2024-01-30 14:08:35 +0530

    added get flight api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -11,6 +11,9 @@ router.post('/', FlightMiddleware.validateCreateRequest, FlightController.create
 // /api/v1/flights?trips=MUM-DEL GET
 router.get('/', FlightController.getAllFlights);
 
+// /api/v1/flights:id GET
+router.get('/:id', FlightController.getFlight);
+
 
 module.exports = router;
 
